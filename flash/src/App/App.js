560ff257c43ef978f3d9cc496a55cf7cebe8c7b9
tariba <tariqxa@gmail.com>
2022-11-23 10:01:08 +0000

    linking dont

--- flash/src/App/App.js
+++ flash/src/App/App.js
@@ -3,40 +3,39 @@ import NavBar from "../NavBar/NavBar";
 import Cards from "../Cards/Cards";
 import QCard from '../Cards/Card/Qcard';
 import ACard from '../Cards/Card/ACard';
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 
 function App() {
   // const [type, setType] = useState("random");
+
+ 
   
-  const [randomQA, setRandomQA] = useState({question : `How do you learn to code?'`, answer: 'Flash it!!!'});
+  const [randomQA, setRandomQA] = useState([{question : `How do you learn to code?'`, answer: 'Flash it!!!'}]);
   const [flip, setFlip] = useState(false);
 
-  async function getRandomQuestion () {
-
+  
+  async function getData(id) {
+    await axios.get(`http://localhost:3001/questions/${id}`).then((response)=>{
+      setRandomQA(response.data.payload);
+      console.log(randomQA)
+    })
+   
     
-
   }
 
-  let flashcards = [
-    { id: "abc1", question: "question1", answer: "answer1" },
-    { id: "abc1", question: "question2", answer: "answer2" },
-    { id: "abc1", question: "question3", answer: "answer3" },
-  ];
-
-  function getData() {
-    //const response = await fetch(`URL/type=${type}`);
-    const index = Math.floor(Math.random() * flashcards.length);
-    //const data = await response.json();
-    setRandomQA(flashcards[index]);
-  }
 
   // function handleClick(event) {
   //   setType(event.target.text);
   // }
 
-  function nextClick() {
-    let data = getData();
+  async function nextClick() {
+     const index = Math.floor(Math.random() * 22);
+     console.log(index)
+    let data = await getData(index);
     console.log("Button clicked");
+    console.log(randomQA[0].question)
     return data;
   }
 
@@ -55,9 +54,9 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Cards question={randomQA.question} answer={randomQA.answer} nextClick={nextClick}>
-        <QCard id='questionCard' Qtext = {randomQA.question}/>
-        <ACard id='answerCard' Atext = {randomQA.answer} onClick = {flipIt} flipped = {flip}/>
+      <Cards>
+        <QCard id='questionCard' Qtext = {randomQA[0].question}/>
+        <ACard id='answerCard' Atext = {randomQA[0].answer} onClick = {flipIt} flipped = {flip}/>
         <button onClick ={handleClick} >Next Question</button>
       </Cards>
     </div>
