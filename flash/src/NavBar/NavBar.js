e74ed455f62e8a854797793d63e80fcfc73a7c08
btxoana <beatrice.stanila@gmail.com>
2022-11-24 16:50:10 +0000

    NavBar testing done

--- flash/src/NavBar/NavBar.js
+++ flash/src/NavBar/NavBar.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./NavBar.css";
-import img from "./KcnaXGx6i.png"
+import img from "./KcnaXGx6i.png";
 
 function NavBar({ navClick }) {
   return (
-    <div id='head'>
-      <div id='image'>
-        <img width="250px"
-          src={img}
-          alt="logo"
-        />
+    <div id="head">
+      <div id="image">
+        <img src={img} alt="logo" />
       </div>
       <nav id="nav-bar">
         <button
---
+++ flash/src/NavBar/NavBar.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import { test, expect } from "@jest/globals";
+
+import NavBar from "./NavBar.js";
+
+test("Technical button renders", () => {
+  render(<NavBar buttonText="Technical" />);
+  const button = screen.getByText("Technical");
+  expect(button).toBeInTheDocument();
+  expect(button).toHaveTextContent("Technical");
+});
+
+test("Behavioural Button", () => {
+  render(<NavBar buttonText="Behavioural" />);
+  const button = screen.getByText("Behavioural");
+  expect(button).toBeTruthy();
+  expect(button).toHaveTextContent("Behavioural");
+});
+
+test("Random image", () => {
+  render(<NavBar buttonText="Random" />);
+  const button = screen.getByText("Random");
+  expect(button).toBeInTheDocument();
+});
+
+test("Renders Image Logo", () => {
+  render(<NavBar />);
+  const image = screen.getByAltText("logo");
+  expect(image).toBeInTheDocument();
+});
