0ccb5f368bec0d4d973e20484ee897a2b4fef88e
jasonchalangary <jason42@tuta.io>
2022-11-24 08:45:30 +0000

    completed css??

--- flash/src/Cards/Card/ACard.js
+++ flash/src/Cards/Card/ACard.js
@@ -4,7 +4,7 @@ import "./ACard.css";
 export default function Card({ Atext, flipped, onClick }) {
   return (
     <div onClick={onClick}>
-      {flipped ? <p>{Atext}</p> : <p id="paragraph">Show Answer</p>}
+      {flipped ? <p>{Atext}</p> : <p id="paragraph">Click here to show answer</p>}
     </div>
   );
 }
